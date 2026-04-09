e8a1b5a949e97ec439a2dbbbaf81a6ccaba888f6
Weibo-student <welin@smail.pembinatrails.ca>
2026-04-09 10:27:06 -0500

    Create the code for the datatype

--- variables.sh
+++ variables.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-"""
+
+: <<'COMMENT'
 This is a Bash script for  variables.
 In this script, I will try to explore everything that I can do with the variables.
 I'm also going to use some different types of variables
-"""
+COMMENT
 
+data_type_example1="1"
+data_type_example2="s"
+echo $data_type_example1
+echo $data_type_example2
