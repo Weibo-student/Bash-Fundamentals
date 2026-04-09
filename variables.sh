ac8b2f1757da5bfc0efb3899a01d4a169f33c2d2
Weibo-student <welin@smail.pembinatrails.ca>
2026-04-09 10:15:05 -0500

    explain the concept

---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+"""
+This is a Bash script for  variables.
+In this script, I will try to explore everything that I can do with the variables.
+I'm also going to use some different types of variables
+"""
+
