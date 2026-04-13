e809c00af328ccc22fac130ffd993d811df300c9
Weibo-student <welin@smail.pembinatrails.ca>
2026-04-13 10:28:09 -0500

    add the concept explaining for quotation

---
+++ quotations.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+: <<'COMMENT'
+Quoting in Bash is a mechanism used to remove the special meaning of certain characters or words to the shell.
+COMMENT
+
+: <<'COMMENT'
+There are two main types of the quotation:
+
+Single Quotes(' '): it provides full quoting. 
+Every cahracter within the quotes will be showed literally.
+
+Double Quotes(" "): it provides partial quoting. 
+It shows the literal value of most characters but still allow the shell to have the dollar sign, backticks and backslashes.
+COMMENT
+
