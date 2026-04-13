486e44ca11d2de6d56d1bf763d4c3ff337fad533
Weibo-student <welin@smail.pembinatrails.ca>
2026-04-13 10:15:42 -0500

    finish the variable

--- variables.sh
+++ variables.sh
@@ -2,18 +2,18 @@
 
 : <<'COMMENT'
 A variable in programming is a named container or placeholder used to store data in a computer's memory that can be accessed, updated, or manipulated during a program's execution.
-Imagine the variable is a box, what we do with variables is just 
+Imagine the variable is a box, what we do with variables is put another thing into this box.
+The name of the variable could be long, but it must be clear enough to make everyone understand what this box have inside
 COMMENT
 
 : <<'COMMENT'
 This is a Bash script for  variables.
-In this script, I will try to explore everything that I can do with the variables.
-I'm also going to use some different types of variables
+In this script, I will try to set, call and echo a variables
 COMMENT
 
-# In Bash, there is not data type, so you could put anything no matter is integer or strings into the variable
-data_type_example1="1"
-data_type_example2="s"
-echo $data_type_example1
-echo $data_type_example2
+# This is how to set the variable in bash
+example1="example"
 
+# This is how to call the variable in bash, we must put the $ before the variable name
+# This is also how to echo the variable, it will print the varible in the terminal.
+echo $example1
