e3febd03358097334b36de3169f13fe52768ed1c
Weibo0123 <weibolin322@gmail.com>
2026-04-14 10:43:07 -0500

    add the concept for the printf

---
+++ printf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+: <<'COMMENT'
+The command "printf" is a built-in command used to display formatted text and data in the terminal
+It is more powerful and consistent than the echo command because it provides precise control over how strings, numbers, and dates are displayed.
+COMMENT
+
