239ccad48c0fb0f93706432aeb8466e5bfcdfe98
Weibo0123 <weibolin322@gmail.com>
2026-04-14 10:15:40 -0500

    finishth equotation

--- quotations.sh
+++ quotations.sh
@@ -14,3 +14,16 @@ Double Quotes(" "): it provides partial quoting.
 It shows the literal value of most characters but still allow the shell to have the dollar sign, backticks and backslashes.
 COMMENT
 
+# It has no differnece when we have the plain text
+echo "example1"
+echo 'example2'
+
+# However, when we try to put a variable inside the double quote,
+# it will output what stores in the variable, but the single quote won't
+quotation_example="example1"
+quotation_example1="echo $quotation_example"
+quotation_example2='echo $quotation_example'
+
+$quotation_example1
+$quotation_example2
+echo "$quotation_example"
