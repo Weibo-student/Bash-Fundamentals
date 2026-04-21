c0383b4d7ea1a458acc3e30eb85e798ae7a918f2
Weibo0123 <weibolin322@gmail.com>
2026-04-21 10:45:44 -0500

    add the commen issus to the printf

--- printf.sh
+++ printf.sh
@@ -5,6 +5,10 @@ The command "printf" is a built-in command used to display formatted text and da
 It is more powerful and consistent than the echo command because it provides precise control over how strings, numbers, and dates are displayed.
 COMMENT
 
+# printf could print \n as a new line, but echo can only print it with -e
 printf "\nhello\n\n"
 echo "\nhello\n"
 echo -e "\nhello\n"
+
+# Most of the commen functions are the the same for echo and printf
+# The comment issues is messing up with the / and \ or don't know aobut which parameter to use
