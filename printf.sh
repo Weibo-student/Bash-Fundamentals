dd00df606f8dd1203818ffc5b12ca83f72ef801e
Weibo0123 <weibolin322@gmail.com>
2026-04-21 10:10:39 -0500

    add compration between printf and echo

--- printf.sh
+++ printf.sh
@@ -5,3 +5,6 @@ The command "printf" is a built-in command used to display formatted text and da
 It is more powerful and consistent than the echo command because it provides precise control over how strings, numbers, and dates are displayed.
 COMMENT
 
+printf "\nhello\n\n"
+echo "\nhello\n"
+echo -e "\nhello\n"
