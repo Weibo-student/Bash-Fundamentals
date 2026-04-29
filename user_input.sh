6ac55a51874c59cbb2a69f296b4c8c02c539aaed
Weibo0123 <weibolin322@gmail.com>
2026-04-29 15:34:03 -0500

    add the read without any parameters

--- user_input.sh
+++ user_input.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 : <<'COMMENT'
-The primary way to get user input in a Bash script is the built-in read command, 
+The primary way to get user input in a Bash script is the built-in "read" command, 
 which captures input from the terminal and stores it in a variable.
 COMMENT
+
+# Use read without any parameters to get user input and put it into the variable
+read example
+echo $example
+# But it doesn't come with any prompts which might confuse the users
 
