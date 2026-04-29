22d54d28ed7d296584c0a3609aff584b5dcf5b7b
Weibo0123 <weibolin322@gmail.com>
2026-04-29 11:35:13 -0500

    finisht eh bracket

--- brackets.sh
+++ brackets.sh
@@ -20,3 +20,12 @@ echo $x
 #The second echo print nothing because the variable x only decleared in the subshell
 # This is the doble round bracket, it's for the math operations
 echo $(( 3 + 5 ))
+
+# The last type is the Curly Brackets {}
+# The curly brackets is sued to create a function
+example_function(){
+    echo "This is a example"
+}
+example_function
+
+# Common issue / msitakes: I think it's common to forget about putting space between brackets and other things inside
