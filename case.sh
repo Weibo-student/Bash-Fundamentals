63f99584c8d3e3d7f992a01a8dd2cb1370b1dc34
Weibo0123 <weibolin322@gmail.com>
2026-04-30 09:55:11 -0500

    add the case statement example

--- case.sh
+++ case.sh
@@ -4,3 +4,22 @@
 A case statement in Bash is a conditional control structure,
 used to execute specific blocks of code based on whether a value matches a given pattern
 COMMENT
+
+# Creates a variable for the case statement
+read -p "Enter a number: " example
+
+# The case statement will echo different thing based on what in the $example
+case "$example" in
+    # If the example is "1"
+  1)
+    echo "The number is one"
+    ;;
+    # If the example is "2"
+  2)
+    echo "The number is two"
+    ;;
+    # If the example is other things
+  *)
+    echo "It's other numbers"
+    ;;
+esac
