d9834ee859b9c3f36165c69041fa52ab6823d759
Weibo0123 <weibolin322@gmail.com>
2026-04-30 10:17:07 -0500

    add the concept to the if statement

---
+++ if_elif_else.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+: <<'COMMENT'
+If statement is a control structure used to execute code blocks based on whether a specific condition or command succeeds.
+It's very similar to the case statement but it's bettet to handle the situation when you fewer conditions
+COMMENT
