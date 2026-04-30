036a5b00334abd095c25e2ed826bb596c1b018cd
Weibo0123 <weibolin322@gmail.com>
2026-04-30 10:08:29 -0500

    add the code to call the arrey

--- arrays.sh
+++ arrays.sh
@@ -4,7 +4,11 @@
 An array is a data structure used to store a collection of multiple values (elements) under a single variable name.
 COMMENT
 
-# This how you can create an arrey
+# This is how you can create an arrey
 example=(example1 example2 example3)
 # You should have a space between each element
 
+# This is how you call an arrwy
+echo ${example[0]}
+# You need the curly brackets surrounds the arrey
+# The number you put in the square is which elements do you want, 0 is the first one.
