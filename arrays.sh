454e4286b279580c505342f3e75b5aa9617d7f54
Weibo0123 <weibolin322@gmail.com>
2026-04-30 10:03:58 -0500

    add the code to create an arrey

--- arrays.sh
+++ arrays.sh
@@ -3,3 +3,8 @@
 : <<'COMMENT'
 An array is a data structure used to store a collection of multiple values (elements) under a single variable name.
 COMMENT
+
+# This how you can create an arrey
+example=(example1 example2 example3)
+# You should have a space between each element
+
