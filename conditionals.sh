e273b96c57ad6c049b660a0b0796c63ea8eb9fbf
Weibo0123 <weibolin322@gmail.com>
2026-05-01 10:07:41 -0500

    create the demo code

--- conditionals.sh
+++ conditionals.sh
@@ -4,3 +4,27 @@
 Bash conditionals are control structures that execute code 
 based on whether a command's exit status is true (0) or false (non-zero).
 COMMENT
+
+: <<'COMMENT'
+-gt: greater than
+-le: less than
+-ge: greater than or equal to
+-le: less than or equal to
+-eq: equal to
+-ne: not equal to
+COMMENT
+
+# Get two variables from user input
+read -p "please enter the one number: " example1
+read -p "please enter another number: " example2
+
+# If the number in example1 is greater than example2
+if [ $example1 -gt $example2 ]; then
+    echo "example1 is greater than example2"
+# 
+elif [ $example1 -lt $example2 ]; then
+    echo "example1 is less than example2"
+elif [ $example1 -eq $example2 ]; then
+    echo "two numbers are the same"
+fi
+
