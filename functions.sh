f1f8b0dadde6a9426db91abc1864bfffbf13ee28
Weibo0123 <230587150+Weibo-student@users.noreply.github.com>
2026-05-05 10:22:59 -0500

    add the standard way to define the function

--- functions.sh
+++ functions.sh
@@ -6,3 +6,8 @@ In bash, functions are blocks of code that group serveral commands under the sam
 Once defined, you can call them anywhere in your script just like a regular command, 
 which helps reduce repetition and makes scripts easier to maintain. 
 COMMENT
+
+# This is the standard way to define a function
+example_function1(){
+    echo "This is example function 1"
+}
