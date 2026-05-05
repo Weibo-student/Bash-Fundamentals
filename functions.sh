86d1870e5a8d34f28189d5c008560f61444f0968
Weibo0123 <230587150+Weibo-student@users.noreply.github.com>
2026-05-05 10:20:23 -0500

    add the concept of functions

---
+++ functions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+: <<'COMMENT'
+In bash, functions are blocks of code that group serveral commands under the same name
+
+Once defined, you can call them anywhere in your script just like a regular command, 
+which helps reduce repetition and makes scripts easier to maintain. 
+COMMENT
