5e01cb38897f776ffe27bf87662893f0ff908dbf
Weibo0123 <230587150+Weibo-student@users.noreply.github.com>
2026-05-09 16:20:00 -0500

    add the concept for while loop(forget to save last time)

--- while.sh
+++ while.sh
@@ -5,3 +5,4 @@ In Bash, while loop is a control flow statement.
 It allows you to execute a block of commands repeatedly as long as a specified condition remains true. 
 Once the condition evaluates to false (an exit status other than zero), the loop terminates.
 COMMENT
+
