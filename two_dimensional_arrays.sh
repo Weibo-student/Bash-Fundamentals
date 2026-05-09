fa5fe8a07e8d994ab861c11fc4d430687c787e8c
Weibo0123 <230587150+Weibo-student@users.noreply.github.com>
2026-05-09 16:46:00 -0500

    add the concept of 2d array

---
+++ two_dimensional_arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+: <<'COMMENT'
+A two-dimensional array is a data structure that organizes elements in a grid or matrix format using rows and columns.
+But bash does not support multi-dimensional arrays.
+COMMENT
+
+
