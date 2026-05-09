fd5a7cebcc47a22f45444f107d7788854bf91769
Weibo0123 <230587150+Weibo-student@users.noreply.github.com>
2026-05-09 16:19:39 -0500

    add the concept for while loop

---
+++ while.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+: <<'COMMENT'
+In Bash, while loop is a control flow statement.
+It allows you to execute a block of commands repeatedly as long as a specified condition remains true. 
+Once the condition evaluates to false (an exit status other than zero), the loop terminates.
+COMMENT
