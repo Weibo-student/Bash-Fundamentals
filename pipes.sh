708fe2c3840e140fbef11d168d1ac163adcc858d
Weibo0123 <230587150+Weibo-student@users.noreply.github.com>
2026-05-09 17:56:37 -0500

    add the first example

--- pipes.sh
+++ pipes.sh
@@ -5,3 +5,6 @@ In Bash, a pipe (|) is a form of redirection that connects the output of one com
 It allows you to chain multiple simple programs together to perform complex data processing without needing to save intermediate results to temporary files.
 COMMENT
 
+# You can use pipe to do different commands in the same time
+printf "hello \nworld" | grep world
+# In the command above, it only outputs world because after outputing the hello \n world, world is the only thing that grep is looking for
