6c1d60b33980341b33a3711ce0126fdaaa5456b9
Weibo0123 <230587150+Weibo-student@users.noreply.github.com>
2026-05-09 17:50:44 -0500

    add the concept to the pipes

---
+++ pipes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+: <<'COMMENT'
+In Bash, a pipe (|) is a form of redirection that connects the output of one command directly to the input of another.
+It allows you to chain multiple simple programs together to perform complex data processing without needing to save intermediate results to temporary files.
+COMMENT
+
