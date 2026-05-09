6133b4153d5e31630f760152f0fb4ff4f30a784f
Weibo0123 <230587150+Weibo-student@users.noreply.github.com>
2026-05-09 16:46:53 -0500

    add an exmple for 2D array

--- two_dimensional_arrays.sh
+++ two_dimensional_arrays.sh
@@ -6,3 +6,11 @@ But bash does not support multi-dimensional arrays.
 COMMENT
 
 
+# Although there's no 2D array in bash, we could use the associative array to replace it.
+declare -A example_2d_arrey
+
+# The 0,0 in the bracket is not actually the number, it's just a string that looks like a 2d arrey
+example_2d_arrey[0,0]=1
+example_2d_arrey[0,1]=2
+example_2d_arrey[1,0]=3
+example_2d_arrey[1,1]=4
