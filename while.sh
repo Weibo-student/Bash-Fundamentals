b0ed66b30f6a723ea951d0b6583e647d128a7eeb
Weibo0123 <230587150+Weibo-student@users.noreply.github.com>
2026-05-09 16:22:45 -0500

    add the firsr example

--- while.sh
+++ while.sh
@@ -6,3 +6,11 @@ It allows you to execute a block of commands repeatedly as long as a specified c
 Once the condition evaluates to false (an exit status other than zero), the loop terminates.
 COMMENT
 
+# To make a while loop, you need to create a variable for the while loop terminate
+example_num=1
+# As long as the number is less than 5, it will keep running
+while [ $example_num -le 5 ]
+do
+    echo "It's $example_num"
+    ((example_num++)) # Increment the number
+done
