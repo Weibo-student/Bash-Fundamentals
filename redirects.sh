ca2d68880d2f45a31ecb08199ecd8f0177416276
Weibo0123 <230587150+Weibo-student@users.noreply.github.com>
2026-05-09 18:09:46 -0500

    add the concept

---
+++ redirects.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+: <<'COMMENT'
+In Bash, redirection (>) is the process of changing the standard locations where a command gets its input or sends its output.
+It allows you to send command output (standard output or standard error) to files or other commands instead of your terminal.
+COMMENT
