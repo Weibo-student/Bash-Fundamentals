1fbea0ddf3493a2fc10affc83067b5ab180fa13d
Weibo0123 <230587150+Weibo-student@users.noreply.github.com>
2026-05-09 22:01:33 -0500

    add the concept of for

---
+++ for.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+: <<'COMMENT'
+In Bash, a for loop is a control flow statement used to automate repetitive tasks by executing a set of commands once for each item in a specified list.
+COMMENT
+
