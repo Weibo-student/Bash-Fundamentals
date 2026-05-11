27197cc3c41e51d770992d1179a818b98e4065a0
Weibo0123 <230587150+Weibo-student@users.noreply.github.com>
2026-05-10 21:45:36 -0500

    add a C-Style for loop example

--- for.sh
+++ for.sh
@@ -5,14 +5,14 @@ In Bash, a for loop is a control flow statement used to automate repetitive task
 COMMENT
 
 # This is a basic bash for loop
-for i in 1 2 3 4
+for i in 0 1 2 3 4
 do
     echo $i
 done
 # You have to provide a list after the variable, it could could any data type (They will all trun into strings anyway)
 
 # You could also use a array
-example_arr=("1" "2" "3" "4")
+example_arr=("0" "1" "2" "3" "4")
 
 for i in "${example_arr[@]}"
 do
@@ -20,7 +20,7 @@ do
 done
 
 # You could also use range like python
-for i in {1..4}
+for i in {0..4}
 do
     echo $i
 done
@@ -28,7 +28,7 @@ done
 # create a folder and file for example
 mkdir -p examples
 touch ./examples/for_example.txt
-printf "1\n2\n3\n4" > ./examples/for_example.txt
+printf "0\n1\n2\n3\n4" > ./examples/for_example.txt
 # You could also use a for loop to read all the things in a file
 for i in $(cat ./examples/for_example.txt)
 do
@@ -46,4 +46,10 @@ do
         # You could use break to break the loop
         break
     fi
+done
+
+# You could also create a C-Style for Loop
+for ((i=0; i<5; i++))
+do 
+    echo $i
 done
